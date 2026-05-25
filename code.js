5bf46c5ec669e6f8acc4f4be10336087da1428cd
Devon Foster <devonfoster@Devons-MacBook-Pro.local>
2026-05-24 23:07:01 -0400

    game structure added

--- code.js
+++ code.js
@@ -1,3 +1,10 @@
-function updateText(){
-    document.getElementById("heading").innerHTML = "Javascript rocks!";
+function Roll Dice(){
+    //this is how a die can be rolled in js and dedicated as die1 so it can be used later by its name
+    let die1 = Math.floor(Math.random() * 6) + 1;
+    //same but for die2
+    let die2 = Math.floor(Math.random() * 6) + 1;
+    //sum of die1 and die2
+    let sum = die1 + die2;
+    //output the result of the roll to the gameoutput element in the html
+    document.getElementById("gameoutput").innerHTML = "You rolled a " + die1 + " and a " + die2 + " for a total of " + sum;
 }
