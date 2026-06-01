a021083181ac18602f41718deebb21657e18e6ad
Devon Foster <devonfoster@Devons-MacBook-Pro.local>
2026-06-01 00:12:39 -0400

    dedbug mistake

--- code.js
+++ code.js
@@ -3,7 +3,7 @@ function rollDice(event){
     //this is how a die can be rolled in js and dedicated as die1 so it can be used later by its name
     let die1 = Math.floor(Math.random() * 6) + 1;
     //same but for die2
-    let die2 = Math.floor(Math.random() * 6) + 1;
+    let die2 = Math.random() * 6 + 1;
     //sum of die1 and die2
     let sum = die1 + die2;
        //output the result of the roll to the gameoutput elements in the html
